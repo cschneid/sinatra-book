e610ca09616592de7ef483065eda4f3b71c1c4eb
Zachary Scott <zachary@zacharyscott.net>
2012-09-07 20:51:20 -0400

    use print.css, add favicon

--- app.rb
+++ app.rb
@@ -8,6 +8,7 @@ get('/'){ erb build }
 get('/download'){ build(true); send_file "#{OUTPUT_DIR}/sinatra-book.pdf", :type => 'application/pdf'; }
 
 get('/book.css') { send_file "#{ASSETS_DIR}/book.css", :type => 'text/css' }
+get('/print.css') { send_file "#{ASSETS_DIR}/print.css", :type => 'text/css' }
 
 get('/download.png') { send_file "#{ASSETS_DIR}/images/download.png", :type => :png }
 
@@ -16,7 +17,9 @@ __END__
 <html>
   <head>
     <title>Sinatra Book</title>
-    <link rel="stylesheet" type="text/css" href="/book.css" />
+    <link rel="stylesheet" type="text/css" href="/book.css" media="screen, projection" />
+    <link rel="stylesheet" type="text/css" href="/print.css" media="print" />
+    <link rel="shortcut icon" href="https://github.com/sinatra/resources/raw/master/logo/favicon.ico">
   </head>
   <body>
     <div id="download">
